9a7583619b74b1caaf7f5c561d9ce1c8bbf85603
Olsson Jesper <SEOLSSONJESP@tetrapak.com>
2021-12-01 16:32:04 +0100

    Day one

---
+++ AoC/day1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+namespace AoC
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(problem1(@"C:\Users\SEOLSSONJESP\source\repos\AoC\AoC\day1\input.txt"));
+            Console.WriteLine(problem2(@"C:\Users\SEOLSSONJESP\source\repos\AoC\AoC\day1\input.txt"));
+        }
+
+        public static int problem1(string filename)
+        {
+            string[] lines = File.ReadAllLines(filename);
+            int count = 0;
+            for (int i = 1; i < lines.Length; i++)
+            {
+                if (int.Parse(lines[i]) > int.Parse(lines[i - 1])) count++;
+            }
+            return count;
+        }
+        public static int problem2(string filename)
+        {
+            string[] lines = File.ReadAllLines(filename);
+            int count = 0;
+            for (int i = 1; i < lines.Length - 2; i++)
+            {
+                if (int.Parse(lines[i-1]) + int.Parse(lines[i]) + int.Parse(lines[i + 1]) < int.Parse(lines[i]) + int.Parse(lines[i + 1]) + int.Parse(lines[i + 2])) count++;
+            }
+            return count;
+        }
+    }
+}
